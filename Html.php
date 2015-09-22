02583cb32e9b38ee225bec50979a48dacc5fd2b2
allarkikkas <allar.kikkas@khk.ee>
2015-09-22 11:27:22 +0300

    tegemisel

--- Html.php
+++ Html.php
@@ -6,18 +6,47 @@
 </head>
 <body>
 <header>
+
+    <!-- pealkirjad -->
+
     <h1>Kõik tagid sissejuhatus</h1>
     <h2>väiksemad tagid</h2>
-    <h5>suhe väiksed</h5>
+    <h3>suhe väiksed</h3>
+    <h4>pealkiri 4</h4>
+    <h5>pealkiri 5 </h5>
+    <h6>pealkiri6</h6>
+    <hr>
 
+
+
+    <!--Lingid-->
+
+    <a>leht</a>
+    <a href="www.youtube.com">youtube link</a>
 </header>
 
+<hr>
+<!--paragrahvid-->
+
+
+<p>
+    Paragrahv<br>siin on bri kasutatud<br>see kaa
+    </br>
+</p>
+<hr>
+
+<!--article-->
+<hr>
+<article>
+    <h1>See leht</h1>
+    <p><b>see on proovi värk</p></b>
+</article>
+<hr>
+
+<aside>
+    <h4>see ha side all</h4>
+    <p>asideee</p>
+</aside>
 
 </body>
 </html>
-/**
-* Created by PhpStorm.
-* User: allarkikkas
-* Date: 16/09/15
-* Time: 11:07
-*/
