0b392c0f4d84f505d024aa5da8da774a2a58557f
allarkikkas <allar.kikkas@khk.ee>
2015-09-22 09:11:12 +0300

    tagid

---
+++ Html.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>fsfsd</title>
+</head>
+<body>
+<header>
+    <h1>Kõik tagid sissejuhatus</h1>
+    <h2>väiksemad tagid</h2>
+    <h5>suhe väiksed</h5>
+
+</header>
+
+
+</body>
+</html>
+/**
+* Created by PhpStorm.
+* User: allarkikkas
+* Date: 16/09/15
+* Time: 11:07
+*/
