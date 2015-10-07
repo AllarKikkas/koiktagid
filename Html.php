6fdf0d924432eeb6bcb5c0908d6a6239a78a3f9c
allarkikkas <allar.kikkas@khk.ee>
2015-10-07 12:32:12 +0300

    tegemisel

--- Html.php
+++ Html.php
@@ -1,26 +1,27 @@
 <!doctype html>
 <html lang="en">
 <head>
+    <link type="text/css" rel="stylesheet" href="kujundus.css"/>
     <meta charset="UTF-8">
     <title>fsfsd</title>
 </head>
 <body>
 <header>dasdsa</header>
-    <!-- pealkirjad -->
+<!-- pealkirjad -->
 
-    <h1>Kõik tagid sissejuhatus</h1>
-    <h2>väiksemad tagid</h2>
-    <h3>suhe väiksed</h3>
-    <h4>pealkiri 4</h4>
-    <h5>pealkiri 5 </h5>
-    <h6>pealkiri6</h6>
-    <hr>
+<h1>Kõik tagid sissejuhatus</h1>
+<h2>väiksemad tagid</h2>
+<h3>suhe väiksed</h3>
+<h4>pealkiri 4</h4>
+<h5>pealkiri 5 </h5>
+<h6>pealkiri6</h6>
+<hr>
 
 
-    <!--Lingid-->
+<!--Lingid-->
 
-    <a>leht</a>
-    <a href="www.youtube.com">youtube link</a>
+<a>leht</a>
+<a href="www.youtube.com">youtube link</a>
 
 
 <hr>
@@ -34,7 +35,7 @@
 <hr>
 
 <!--article-->
-<hr>
+
 <article>
     <h1>See leht</h1>
     <p><b>see on proovi värk</p></b>
@@ -45,6 +46,299 @@
     <h4>see ha side all</h4>
     <p>asideee</p>
 </aside>
+
+<img src="http://images4.fanpop.com/image/photos/24000000/New-SEXY-VS-miranda-kerr-24077139-760-1024.jpg" width="360" height="300" usemap="#hiireke">
+
+
+<br>
+
+<b>Example</b>
+
+<br>
+
+<center>This will center your contents</center>
+<br><br><br>
+
+
+<dl>
+    <dt>Definition Term</dt> <dd>Definition of the term</dd> <dt>Definition Term</dt> <dd>Definition of the term</dd> </dl>
+
+<br><br><br>
+
+<dl>
+<dt>Definition Term</dt> <dd>Definition of the term</dd> <dt>Definition Term</dt> <dd>Definition of the term</dd> </dl>
+
+<br><br<br><br>
+
+<dl>
+    <dt>Definition Term</dt>
+    <dd>Definition of the term</dd>
+    <dt>Definition Term</dt>
+    <dd>Definition of the term</dd> </dl>
+
+<br><br<br><br>
+
+This is an <em>Example</em> of using the emphasis tag
+
+<br><br<br><br>
+
+<embed src="yourfile.mid" width="100%" height="60" align="center">
+    <br><br<br><br>
+
+<embed src="yourfile.mid" autostart="true" hidden="false" loop="false"> <noembed><bgsound src="yourfile.mid" loop="1"></noembed>
+    <br><br<br><br>
+
+
+    <font face="Times New Roman">Example</font>
+
+    <br><br<br><br>
+
+
+    <form action="mailto:allarkikkas@hotmail.com ">
+        Name: <input name="Name" value="" size="10"><br>
+        Email: <input name="Email" value="" size="10"><br>
+        <center><input type="submit"></center> </form>
+
+<br><br><br>
+
+    <hr width="50%" size="3" noshade />
+
+
+<br><br><br>
+
+    <i>Example</i>
+
+    <br><br><br>
+
+    <form method=post action="/cgi- bin/example.cgi">
+        <input type="text" size="10" maxlength="30">
+        <input type="Submit" value="Submit"> </form>
+
+<br>
+
+    <form method=post action="/cgi- bin/example.cgi">
+        Select an option:<br>
+        <input type="radio" name="option"> Option 1
+        <input type="radio" name="option" checked> Option 2
+        <input type="radio" name="option"> Option 3
+        <br>
+        <br>
+        Select an option:<br>
+        <input type="checkbox" name="selection"> Selection 1
+        <input type="checkbox" name="selection" checked> Selection 2
+        <input type="checkbox"
+               name="selection"> Selection 3
+        <input type="Submit" value="Submit"> </form>
+
+<br>
+
+    <marquee bgcolor="#cccccc" loop="50" scrollamount="50" width="100%">Example Marquee</marquee>
+
+    <br>
+    <menu>
+        <li type="disc">List item 1</li> <li type="circle">List item 2</li> <li type="square">List item 3</li> </menu>
+
+    <br>
+    <meta name="Description" content="Description of your site"> <meta name="keywords" content="keywords describing your site">
+
+<br>
+
+    <img src="http://cdn.orkin.com/images/rodents/deer-mouse-illustration_360x244.jpg" width="360" height="244" usemap="#hiireke">
+
+    <map name="hiireke">
+        <area shape="circle" coords="121,122,7" href="http://listovative.com/different-eye-colors-say/" alt="Eye color meanings">
+        <area shape="circle" coords="223,40,19" href="https://en.wikipedia.org/wiki/Mouse_(computing)" alt="mouse">
+    </map>
+
+
+<br>
+
+
+    <iframe width="420" height="345"
+            src="http://www.youtube.com/embed/XGSy3_Czz8k">
+    </iframe>
+
+
+    <caption>CAPTION tag adds a caption to a table.</caption>
+
+    <center>CENTER tags center text, images, etc.</center>
+
+
+
+
+
+    <del>deleted dsfjdskjhfhjkdskjtgjkaedgjhadfjkkdatext</del>
+
+<br>
+
+    <p>We open at <time>10:00</time> every morning.</p>
+
+<wbr>sadasda</wbr>
+
+<br>
+
+    <table>
+        <caption>Monthly savings</caption>
+        <colgroup>
+            <col span="3" style="background-color:blue">
+            <col style="background-color:purple">
+        </colgroup>
+        <thead>
+        <tr>
+            <th>Millal
+                <dialog open="">Lahtine aken</dialog>
+            </th>
+            <th>Misasi</th>
+            <th>Kui palju</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>efdsl</td>
+            <td>Mfsdfgi</td>
+        </tr>
+        </tbody>
+        <tfoot>
+        <tr>
+            <td>Millalgi</td>
+            <td>Veokeht</td>
+            <td>Üks</td>
+        </tr>
+        </tfoot>
+    </table>
+
+<br>
+
+
+    <br>
+
+    <form oninput="x.value=parseInt(a.value)+parseInt(b.value)">0
+        <input type="range" id="a" value="50">100
+        +<input type="number" id="b" value="50">
+        =
+        <output name="x" for="a b">142</output>
+    </form>
+
+
+    <br>
+    <meter value="5" min="0" max="10">2 out of 10</meter>
+
+
+
+<br>
+
+
+
+
+    <section>
+        <h1>This is a heading, just because</h1>
+        <span>This is a section of text.</span>
+
+        <p><s>Normal text</s></p>
+
+        <p>
+            <small>Small text</small>
+        </p>
+        <p><u>Different text</u></p>
+
+        <p>See <sub>Subscript</sub> on suht kasulik</p>
+
+        <p>Sama on <sup>Superscripted</sup> tekstiga</p>
+
+        <p>Tunnid lõppevad
+            <time>17:15</time>
+            .
+        </p>
+    </section>
+<br>
+
+
+    <video width="320" height="240" controls="">
+        <source src="http://www.w3schools.com/tags/movie.mp4" type="video/mp4">
+
+        <track src="http://www.subtitles.com" kind="subtitles" srclang="en" label="English"></track>
+        3 tagi selle jaoks, embed/iframe/video?
+    </video>
+
+
+<br>
+    <form>
+        <fieldset>
+            <legend>Personalia:</legend>
+            Name: <input type="text"><br>
+            Email: <input type="text"><br>
+            Date of birth: <input type="text">
+        </fieldset>
+    </form>
+
+    <br>
+    <table>
+        <tr>
+            <th>January <dialog open>This is an open dialog window</dialog></th>
+            <th>February</th>
+            <th>March</th>
+        </tr>
+        <tr>
+            <td>31</td>
+            <td>28</td>
+            <td>31</td>
+        </tr>
+    </table>
+
+<br>
+
+    <details>
+        <summary>Copyright 1999-2014.</summary>
+        <p> - by Refsnes Data. All Rights Reserved.</p>
+        <p>All content and graphics on this web site are the property of the company Refsnes Data.</p>
+    </details>
+
+<br>
+
+    <div id="main">
+        <div style="background-color:coral;">A</div>
+        <div style="background-color:lightblue;">B</div>
+        <div style="background-color:khaki;">C</div>
+        <div style="background-color:pink;">D</div>
+        <div style="background-color:lightgrey;">E</div>
+        <div style="background-color:lightgreen;">F</div>
+    </div>
+
+<br>
+
+    <marquee behavior="alternate"><marquee width="200">Uus Marquee</marquee></marquee>
+
+<br>
+
+<textarea rows="4" cols="30">siia saab kirjutada
+
+</textarea>
+
+<br>
+
+    <pre>preee    taagid</pre>
+
+<br>
+
+
+    <div>
+        <del>Või hoopiski del tag</del>
+        <details>Detailid ilmuvad siia</details>
+        <dfn>DFN tag</dfn>
+        <dialog>Dialog tag</dialog>
+
+        <dl>
+            <dt>Kohv</dt>
+            <dd>Must <s>kuum</s> jook</dd>
+            <dt>Piim</dt>
+            <dd>Valge külm jook</dd>
+        </dl>
+    </div>
+
+
+    <canvas id="essaCanva" width="50" height="50" style="border:5px solid #40cc6f;">Teie browser ei toeta nii uhket tagi
+    </canvas>
+
 
 </body>
 </html>
